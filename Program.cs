6bca7508f3237f3cff0549c2219fdade2b21fd06
kyan <delegatkyan@gmail.com>
2023-03-15 12:31:03 +1300

    added if statement and static void

--- Program.cs
+++ Program.cs
@@ -4,10 +4,31 @@
     {
         static void Main(string[] args)
         {
-            int name1;
+            string name1;
+            int input;
             Console.WriteLine("\t\t\t\t\tWelcome To the Te Reo Quiz!!\n\n\n\n"); //the title which is displayed at the top middle of the screen then new line for the name input.
-            Console.WriteLine("Please enter Name: ");
-
+            Console.Write("Please enter Name: ");
+            name1 = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("\n\n\n\n\n\n\n\n\n\n\t\t\tHello " + name1 + " ,There are Three Different Difficulties you can choose from \n\n\t\t\t(1)Easy\t\t(2)Intermediate\t\t(3)Impossible\t\t(4)Exit");
+            input = Console.ReadLine(); 
+            Console.Clear();
+            if (input == 1) 
+            {
+                EasyMode();
+            }
+            else if (input == 2)
+            {
+                IndermediateMode();
+            }
+            else if (input == 3)
+            {
+                Impossible();
+            }
+            else if (input == 4)
+            {
+                Console.WriteLine("GoodBye, Play again soon!");
+            }
 
 
 
@@ -17,5 +38,19 @@
 
             Console.Read();
         }
+        static void EasyMode() {
+
+            Console.WriteLine("Welcome to Easy Mode. GoodLuck!!");
+
+        }
+        static void IndermediateMode()
+        {
+            Console.WriteLine("Welcome to Intermediate Mode. Have Fun!");
+        }
+        static void Impossible()
+        {
+            Console.WriteLine("Welcome to The Impossible Quiz, Best Of Luck!");
+        }
+       
     }
 }
