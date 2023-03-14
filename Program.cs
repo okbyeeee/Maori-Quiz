37a7bcc1f159ec7e036a86a3c122504d80756413
kyan <delegatkyan@gmail.com>
2023-03-15 11:51:09 +1300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Maori_Quiz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int name1;
+            Console.WriteLine("\t\t\t\t\tWelcome To the Te Reo Quiz!!\n\n\n\n"); //the title which is displayed at the top middle of the screen then new line for the name input.
+            Console.WriteLine("Please enter Name: ");
+
+
+
+
+
+
+
+
+            Console.Read();
+        }
+    }
+}
