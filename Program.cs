cfb8afa11f13c134ce4006a87676648919e888a6
kyan <delegatkyan@gmail.com>
2023-03-21 14:13:24 +1300

    added first question for easy mode.

--- Program.cs
+++ Program.cs
@@ -5,32 +5,43 @@
         static void Main(string[] args)
         {
             string name1;
-            int input;
+            int input, mode;
             Console.WriteLine("\t\t\t\t\tWelcome To the Te Reo Quiz!!\n\n\n\n"); //the title which is displayed at the top middle of the screen then new line for the name input.
             Console.Write("Please enter Name: ");
             name1 = Console.ReadLine();
             Console.Clear();
             Console.WriteLine("\n\n\n\n\n\n\n\n\n\n\t\t\tHello " + name1 + " ,There are Three Different Difficulties you can choose from \n\n\t\t\t(1)Easy\t\t(2)Intermediate\t\t(3)Impossible\t\t(4)Exit");
             input = Convert.ToInt32(Console.ReadLine());
-            Console.Clear();
-            if (input == 1)
-            {
-                EasyMode();
-            }
-            else if (input == 2)
-            {
-                IndermediateMode();
-            }
-            else if (input == 3)
-            {
-                Impossible();
-            }
-            else if (input == 4) 
-            {
-             ExitMode();
-            }
+            
+           
+                if (input == 1)
+                {
+                    EasyMode();
+                }
+                else if (input == 2)
+                {
+                    IndermediateMode();
+                }
+                else if (input == 3)
+                {
+                    Impossible();
+                }
+                else if (input == 4)
+                {
+                    ExitMode();
+                }
+                else if (input == 5)
+                {
+                    Console.WriteLine("Please enter a valid number");
+                }
+                
+            
+              
+                
+            
+            
 
-
+               
 
 
 
@@ -41,22 +52,30 @@
         static void EasyMode()
         {
             //added 3 different levels and a greeting when one is selected
-        System.Console.WriteLine("Welcome to Easy Mode. GoodLuck!!");
-            Console.Clear();
-           
-            Console.WriteLine("");
+            
+        System.Console.WriteLine("\n\n\t\t\t\t\tWelcome to Easy Mode. GoodLuck!!!");
+            
+         
+            Console.WriteLine("\t\t\t\t\tThere Are 5 Questions In Easy mode\n");
+            
+            Console.WriteLine("1)\tThe Maori name for New Zealand means...");
+            Console.WriteLine("(A)Land of the Long White Cloud\n(B)A Country\n(C)White Isle");
+            
 
+
+            Console.Read();
         }
         static void IndermediateMode()
         {
-        System.Console.WriteLine("Welcome to Intermediate Mode. Have Fun!");
+        System.Console.WriteLine("\n\n\t\t\t\t\tWelcome to Intermediate Mode. Have Fun!");
         }
         static void Impossible()
         {
-        System.Console.WriteLine("Welcome to The Impossible Quiz, Best Of Luck!");
+        System.Console.WriteLine("\n\n\t\t\t\tWelcome to The Impossible Quiz, Best Of Luck!");
         } 
         static void ExitMode()
         { System.Console.WriteLine("Are you sure you want to Exit? (Y/N)");
+          
 
           
 
