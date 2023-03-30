f8b52e845cedc3b9e7e35a74b1c34adc1de596a0
kyan <delegatkyan@gmail.com>
2023-03-31 01:01:27 +1300

    added input

--- Program.cs
+++ Program.cs
@@ -1,146 +1,204 @@
-﻿using System.ComponentModel.Design;
-using System.Threading.Channels;
+﻿using System.Runtime.CompilerServices;
 
-namespace Maori_Quiz
+namespace Quiz
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            string name1, answer1;
-            int input, mode;
-            int score = 0;
-            bool isRight = true;
+            Console.WriteLine("\n\n\t\t\t\t\t\tWelcome to the Te Reo Quiz!");//The name of the Quiz and the beautiful first welcome message
+            Console.WriteLine("\n\nPlease enter your name: "); //asks user to Input their name (manners are key)
+            string name1 = Console.ReadLine();
+            Console.WriteLine("\n\n\n\n\n\n\n\t\t\tHello " + name1 + " ,There are Three Different Difficulties you can choose from \n\n\t\t\t(1)Easy\t\t(2)Intermediate\t\t(3)Impossible\t\t(4)Exit"); //welcomes user and asks them to choose their level depending on how smart they are. 
 
-            Console.WriteLine("\t\t\t\t\tWelcome To the Te Reo Quiz!!\n\n\n\n"); //the title which is displayed at the top middle of the screen then new line for the name input.
-            Console.Write("Please enter Name: ");
-            name1 = Console.ReadLine();
-            while (isRight)
+            int Thelevel;
+            while (true)
             {
-                Console.WriteLine("\n\n\n\n\n\n\n\n\n\n\t\t\tHello " + name1 + " ,There are Three Different Difficulties you can choose from \n\n\t\t\t(1)Easy\t\t(2)Intermediate\t\t(3)Impossible\t\t(4)Exit");
-                if (!int.TryParse(Console.ReadLine(), out input))
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out Thelevel))
                 {
-                    Console.WriteLine("Please enter a valid number");
-                }
-                else
-                {
-                    break;
-                }
-
-                if (input == 1)
-                {
-                    EasyMode();
-                }
-                else if (input == 2)
-                {
-                    IndermediateMode();
-                }
-                else if (input == 3)
-                {
-                    Impossible();
-                }
-                else if (input == 4)
-                {
-                    Console.WriteLine("Are you sure you want to exit? (Y/N)");
-                    string exitAnswer = Console.ReadLine();
-                    if (exitAnswer.ToLower() == "y")
+                    if (Thelevel == 1)
                     {
-                        ExitMode();
+                        EasyMode();
                         break;
+                    }
+                    else if (Thelevel == 2)
+                    {
+                        IntermediateMode();
+                        break;
+                    }
+                    else if (Thelevel == 3)
+                    {
+                        ImpossibleMode();
+                        break;
+                    }
+                    else if (Thelevel == 4)
+                    {
+                        Console.WriteLine("Do you want to exit? (y/n)");
+                        string exitInput = Console.ReadLine();
+                        if (exitInput.ToLower() == "y")
+                        {
+                            Environment.Exit(0);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Whatever your doing isnt right. Please enter  1, 2, 3, or 4.\n\n\nTry Again!");//if the user fails to input 1-4 it tells them nicely that its wrong and they need to try again
                     }
                 }
                 else
                 {
-                    Console.WriteLine("Please enter a valid number");
+                    Console.WriteLine("Seriously " + name1 + ". Please just Enter 1, 2, 3, or 4.\n\n\nNot that hard\n Try again :)");
+                    //if they still dont understand the code comes to life a bit and asks them to stop joking around and input a number between 1 and 4
+                }
+            }
+        }//end of main method
+
+        private static void IntermediateMode()
+
+        {
+            Console.WriteLine(".");
+            Console.Clear();
+            Console.Write("\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\tLoading The Intermediate Quiz: ");
+            string[] spinner = new string[] { "/", "-", "\\", "|" };
+            for (int i = 0; i <= 50; i++)
+            {
+                Console.Write(spinner[i % spinner.Length]);
+                System.Threading.Thread.Sleep(100);
+                Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+            }
+            Console.WriteLine("sending you to Indermediate mode");
+            Console.Clear();
+
+            string[] questions = new string[]
+                {
+                "What is the Maori word for 'Sun'?\nA) Rangi\nB) Marama\nC) Ra",//first question
+                "What is the Maori word for 'family'?\nA) Whanau\nB) Kaumatua\nC) Kaitiaki",//second quesion
+                " What is the Maori word for \"wind\"? \nA) Hau\nB) Ahi\nC) Kaha",//third quesion
+                "What is the Maori word for 'Ccean'? \nA) Moana\nB) Awa\nC) Wai",//fourth question
+                "5. What is the Maori word for \"earth\"? \nA) Whenua\nB) Awa\nC) Wai "//last quesion
+                };
+
+            int score = 0;
+
+            for (int i = 0; i < questions.Length; i++)
+            {
+                Console.WriteLine(questions[i]);
+                string answer = Console.ReadLine().ToUpper();
+
+                if (answer == "A" || answer == "B" || answer == "C")
+                {
+                    if (answer == "A")
+                    {
+                        Console.WriteLine("Correct!");
+                        score++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wrong answer!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid answer!");
                 }
             }
 
-            static void EasyMode()
+            Console.WriteLine("Your Final score is: " + score);
+
+        }//end of intermediate mode
+
+        private static void ImpossibleMode()
+        {
+            int score = 0;
+            Console.WriteLine(".");
+            Console.Clear();
+            Console.Write("\n\n\n\n\n\n\n\n\n\n\n\n\t\n\t\t\t\t\t\tLoading Impossible Quiz: ");
+            string[] spinner = new string[] { "/", "-", "\\", "|" };
+            for (int i = 0; i <= 50; i++)
             {
-                //added 3 different levels and a greeting when one is selected
-                System.Console.WriteLine("\n\n\t\t\t\t\tWelcome to Easy Mode. GoodLuck!!!");
-                string answer2;
-                int score = 0;
-                if (int.TryParse(Console.ReadLine(), out score))
-                    // Question 1
-                    Console.WriteLine("What is The Maori name for New Zealand");
-                Console.WriteLine("A) White cloud");
-                Console.WriteLine("B) Aotearoa");
-                Console.WriteLine("C) New Zealand");
-                answer2 = Console.ReadLine().ToUpper();
-                if (answer2 == "B")
-                {
-                    Console.WriteLine("Correct!");
-                    score++;
-                }
-                else
-                {
-                    Console.WriteLine("Incorrect!");
-                }
-                if (answer2 == "C")
-                {
-                    Console.WriteLine("What does Whero mean?");
-                    Console.WriteLine("A) Blue");
-                    Console.WriteLine("B) Green");
-                    Console.WriteLine("C) Red");
-                    answer2 = Console.ReadLine();
-                }
-                // Question 2
-                
-                if (answer2 == "C")
-                {
-                    Console.WriteLine("Correct!");
-                    score++;
-                }
-                else
-                {
-                    Console.WriteLine("Incorrect!");
-                }
-
-                // Question 3
-                Console.WriteLine("How Do you say hello in Maori?");
-                Console.WriteLine("A) Sup G");
-                Console.WriteLine("B) Kia Ora");
-                Console.WriteLine("C) Morena");
-                answer2 = Console.ToUpper();
-                if (answer2 == "B")
-                {
-                    Console.WriteLine("Correct!");
-                    score++;
-                }
-                else
-                {
-                    Console.WriteLine("Incorrect!");
-                }
-
-                // Final score
-                Console.WriteLine("Your final score is: " + score);
-
+                Console.Write(spinner[i % spinner.Length]);
+                System.Threading.Thread.Sleep(100);
+                Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
             }
+            Console.WriteLine("sending you: ");
+            Console.Clear();
+            Console.WriteLine("\n\n\t\t\t\t\t\tWelcome to Impossible Mode\n\n\t\t\t\tThere are Five Questions in this mode! Goodluck");
+            Console.WriteLine("Quesion (1)\n How Do you say Hello in Maori\n(a)Sup G\n(b)Kia Ora\n(c)Hiii");
 
-            static void IndermediateMode()
+
+            Console.Read();
+        }//end of Impossible quiz
+
+        private static void EasyMode()
+        {
+
+            //loading bar that looks cool and counts as an array? 2 in 1
+            Console.Write("Loading The Easy Quiz: ");
+            string[] spinner = new string[] { "/", "-", "\\", "|" };
+            for (int i = 0; i <= 50; i++)
             {
-                System.Console.WriteLine("\n\n\t\t\t\t\tWelcome to Intermediate Mode. Have Fun!");
+                Console.Write(spinner[i % spinner.Length]);
+                System.Threading.Thread.Sleep(100);
+                Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+            }
+            Console.WriteLine("sending you to easy mode");
+            Console.Clear();
+            Console.WriteLine("\n\t\t\t\t\tWelcome to Easy Mode!!\nThere are Five Questions, GoodLuck");
+            {
+                string[] Easyquestions = new string[]
+                {
+                "What does 'kia ora' mean?\nA) Thank you\nB) Hello\nC) Goodbye",
+                "What is the Maori word for 'family'?\nA) Whanau\nB) Kaumatua\nC) Kaitiaki",
+                "What is a 'haka'?\nA) A traditional dance\nB) A type of food\nC) A musical instrument",
+                "What is the name of the Maori god of the forest?\nA) Tane\nB) Maui\nC) Rongo",
+                "What is the Maori word for 'water'?\nA) Wai\nB) Awa\nC) Moana"
+                };
+
                 int score = 0;
-                string[] questions =
+
+                for (int i = 0; i < Easyquestions.Length; i++)
                 {
-                    System.Console.WriteLine("Test");
+                    Console.WriteLine(Easyquestions[i]);
+                    string answer = Console.ReadLine().ToUpper();
+
+                    if (answer == "A" || answer == "B" || answer == "C")
+                    {
+                        if (answer == "B")
+                        {
+                            Console.WriteLine("Correct!");
+                            score++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Wrong answer!");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid answer!");
+                    }
                 }
 
-
-            static void Impossible()
-                {
-                    System.Console.WriteLine("\n\n\t\t\t\tWelcome to The Impossible Quiz, Best Of Luck!");
-                }
-
-                static void ExitMode()
-                {
-                    System.Console.WriteLine("Thank you for playing!");
-                }
+                Console.WriteLine("Your Final score is: " + score);
             }
         }
+
     }
+    //end of easy quiz
+
 }
-    
-    
+ 
+
+
+
+         
+
+
+
+
+      
+
+
+
 
